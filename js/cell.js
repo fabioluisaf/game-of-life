b5bbc282b704b4c9a58f332f7bc6827e5b24b74a
fabioluisaf <fabioluizflaf@gmail.com>
2020-02-17 17:33:48 -0300

    Initial commit

---
+++ js/cell.js
@@ -0,0 +1,22 @@
+class Cell {
+    constructor(x, y, size, state) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.live = state;
+        this.willChange = false;
+    }
+
+    display() {
+        strokeWeight(0);
+        this.live ? fill(0) : fill(255);
+        rect(this.x * this.size + 1, this.y * this.size + 1, this.size - 1, this.size - 1);
+    }
+
+    update() {
+        if(this.willChange) {
+            this.live = !this.live;
+            this.willChange = !this.willChange;
+        }
+    }
+}
---
+++ js/sketch.js
@@ -0,0 +1,107 @@
+const tileSize = 12;
+
+let playButton;
+let resetButton;
+let clearButton;
+let canvas;
+
+let paintMode;
+let label;
+
+let grid;
+
+function setup() {
+    canvas = createCanvas(1201, 601);
+    background(175);
+    noLoop();
+
+    canvas.mousePressed(paintTile);
+
+    paintMode = true;
+    label = document.getElementById("click-type");
+
+    grid = new Grid(tileSize);
+    grid.display();
+
+    playButton = {
+        button: createButton("Play").size(150, 30),
+        state: false
+    };
+    playButton.button.mousePressed(() => {
+        playButton.state = !playButton.state;
+
+        if(playButton.state) {
+            playButton.button.html("Pause");
+            loop();
+        } else {
+            playButton.button.html("Play");
+            noLoop();
+        }
+    });
+
+    resetButton = createButton("Reset").size(150, 30);
+    resetButton.mousePressed(() => {
+        grid = new Grid(tileSize);
+        grid.display();
+
+        playButton.button.html("Play");
+        playButton.state = false;
+        noLoop();
+    });
+
+    clearButton = createButton("Clear Grid").size(150, 30);
+    clearButton.mousePressed(() => {
+        for(let row of grid.layout) {
+            for(let tile of row) {
+                tile.live = false;
+                tile.willChange = false;
+            }
+        }
+        grid.display();
+
+        playButton.button.html("Play");
+        playButton.state = false;
+        noLoop();
+    });
+}
+
+function draw() {
+    grid.nextGen();
+    grid.displayAndUpdate();
+}
+
+function paintTile() {
+    let mX = Math.floor(mouseX / tileSize);
+    let mY = Math.floor(mouseY / tileSize);
+
+    let clickedTile = grid.get(mX, mY);
+
+    if(!clickedTile) {
+        return;
+    }
+
+    if(paintMode) {
+        clickedTile.live = true;
+    } else {
+        clickedTile.live = false;
+    }
+
+    grid.display();
+}
+
+function keyPressed() {
+    if(keyCode === 83) {
+        paintMode = !paintMode;
+
+        if(paintMode) {
+            label.innerHTML ="Draw";
+        } else {
+            label.innerHTML = "Erase";
+        }
+    }
+    console.log(keyCode);
+}
+
+function mouseDragged() {
+    paintTile();
+}
